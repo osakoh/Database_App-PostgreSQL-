8b1a94e841cfc71989871844f30c32a7e6380d42
osakoh <macmive@gmail.com>
2021-03-05 17:58:31 +0000

    created a connection pool with 1 connection as minconn and 10 as maxconn

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
-import psycopg2
+from psycopg2 import pool
 
 from custom_settings import Configure
 
 c = Configure()
 
+# def connect():
+#     return psycopg2.connect(user=c.usr,
+#                             password=c.pwd,
+#                             database=c.db,
+#                             host=c.host)
 
-def connect():
-    return psycopg2.connect(user=c.usr,
-                            password=c.pwd,
-                            database=c.db,
-                            host=c.host)
+
+connection_pool = pool.SimpleConnectionPool(minconn=1, maxconn=10, user=c.usr,
+                                            password=c.pwd, database=c.db, host=c.host
+                                            )
